d6ef63c2b1b710a936643d5a96c11f02b3fa5e69
cristiano <cristiano.saps@gmail.com>
2016-10-09 17:26:03 -0300

    Início de criação do banco de dados. Apenas a tabela sintomas

---
+++ app/src/main/java/com/example/cristiano/homeopatia/banco_de_dados/BancoDeDadosHelper.java
@@ -0,0 +1,31 @@
+package com.example.cristiano.homeopatia.banco_de_dados;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by cristiano on 09/10/16.
+ */
+
+public class BancoDeDadosHelper extends SQLiteOpenHelper {
+
+    //atualização de versao do banco de dados
+    public static final int DATABASE_VERSION = 1;
+    //definição do nome do arquivo
+    public static final String NOMEBANCO = "HOMEOPATIA.sqlite"
+
+    public BancoDeDadosHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, NOMEBANCO, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SintomasContract.SintomasEntry.CRIADOR_TABELA_SINTOMAS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/cristiano/homeopatia/banco_de_dados/SintomasContract.java
@@ -0,0 +1,34 @@
+package com.example.cristiano.homeopatia.banco_de_dados;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by cristiano on 09/10/16.
+ */
+
+public final class SintomasContract {
+    public SintomasContract() {
+    }
+ public static abstract class SintomasEntry implements BaseColumns{
+     public static final String NOME_TABELA = "Sintomas";
+     public static final String COLUNA_ID = "idSintomas";
+     public static final String COLUNA_ENERGETICOS = "Energeticos";
+     public static final String COLUNA_MENTAIS = "Metais";
+     public static final String COLUNA_EMOCIONAIS = "Emocionais";
+     public static final String COLUNA_FISICOS = "Fisicos";
+     public static final String COLUNA_ESPECIFICOS = "Especificos";
+
+     //DEFININDO CODIGO SQL DA TABELA SINTOMAS
+     public static final String CRIADOR_TABELA_SINTOMAS =
+             "CREATE TABLE"+NOME_TABELA+" ("
+                 +COLUNA_ID+" INTEGER UNSIGNED NOT NULL AUTOINCREMENT,"+
+                 COLUNA_ENERGETICOS+" TEXT NULL,"+
+                 COLUNA_MENTAIS+" TEXT NULL,"+
+                 COLUNA_EMOCIONAIS+" TETXT NULL,"+
+                 COLUNA_FISICOS+" TEXT NULL,"+
+                 "PRIMARY KEY("+COLUNA_ID+"));";
+     COLUNA_ESPECIFICOS+" TEXT NULL,"+
+
+ }
+
+}
